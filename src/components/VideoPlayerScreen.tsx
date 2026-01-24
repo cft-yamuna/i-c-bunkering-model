5457803112a6a21bc53514ed69f087fca23f9aad
chetusangolgi <chetusangolgi@gmail.com>
2026-01-24 20:03:08 +0530

    ready

--- src/components/VideoPlayerScreen.tsx
+++ src/components/VideoPlayerScreen.tsx
@@ -18,24 +18,18 @@ export default function VideoPlayerScreen({ videoNumber, onVideoEnd, onBack, onH
 
   return (
     <div className="fixed inset-0 w-full h-full bg-black">
-      <div className="absolute bottom-8 left-0 right-0 z-20 flex justify-between px-6">
+      <div className="absolute bottom-12 left-0 right-0 z-20 flex justify-between px-16">
         <button
           onClick={onBack}
           className="transition-all duration-300 hover:scale-110"
         >
-          <img src="/back.png" alt="Back" className="w-24 h-auto" />
+          <img src="/back.png" alt="Back" className="w-33 h-20" />
         </button>
-        <button
-          onClick={onHome}
-          className="transition-all duration-300 hover:scale-110"
-        >
-          <img src="/home.png" alt="Home" className="w-24 h-auto" />
-        </button>
+       
       </div>
       <video
         ref={videoRef}
         onEnded={onVideoEnd}
-        controls
         className="w-full h-full object-contain"
       >
         <source src={`/${videoNumber}.mp4`} type="video/mp4" />
